8f8ee74f823311a258b738df65b84624bc50a9d3
Nathan Lacroux <nathan.lacroux34@gmail.com>
2023-03-17 15:51:09 +0100

    Implement logic

--- app/src/main/java/com/example/twisterfinger/engine/GameEngine.java
+++ app/src/main/java/com/example/twisterfinger/engine/GameEngine.java
@@ -1,6 +1,17 @@
 package com.example.twisterfinger.engine;
 
+import com.example.twisterfinger.activities.views.objects.Couleur;
+import com.example.twisterfinger.activities.views.objects.TwisterCircle;
+
 public class GameEngine {
+    public void checkGoodCircle(TwisterCircle circleTouched) {
+        if (!circleTouched.getCouleur().equals(rCouleur)) {
+            this.state = StateEnum.GAME_OVER;
+        } else {
+            state = StateEnum.WHEEL;
+        }
+    }
+
     public enum StateEnum {
         WHEEL,
         FINGER,
@@ -10,11 +21,15 @@ public class GameEngine {
 
     private StateEnum state;
 
+    private Couleur rCouleur;
+    private FingerEnum rfinger;
+
+
     public GameEngine() {
         state = StateEnum.WHEEL;
     }
 
-    private void nextState() {
+    public void nextState() {
         switch (state) {
             case WHEEL:
                 state = StateEnum.FINGER;
@@ -27,13 +42,33 @@ public class GameEngine {
         }
     }
 
-    private void freeze() {
+    public void freeze() {
         if (StateEnum.FINGER.equals(state)) {
             state = StateEnum.FREEZE;
         }
     }
 
+    public void gameover() {
+        state = StateEnum.GAME_OVER;
+    }
+
     public StateEnum getState() {
         return state;
+    }
+
+    public Couleur getrCouleur() {
+        return rCouleur;
+    }
+
+    public void setrCouleur(Couleur rCouleur) {
+        this.rCouleur = rCouleur;
+    }
+
+    public FingerEnum getRfinger() {
+        return rfinger;
+    }
+
+    public void setRfinger(FingerEnum rfinger) {
+        this.rfinger = rfinger;
     }
 }
